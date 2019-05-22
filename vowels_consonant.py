b100d5343841df01304b0cd7982ef830253690ab
Muthukamalan <50898904+Muthukamalan@users.noreply.github.com>
2019-05-22 17:53:38 +0530

    Create vowels_consonant.py

---
+++ vowels_consonant.py
@@ -0,0 +1,10 @@
+import string
+x=input()
+x=x.lower()
+v=('a','e','i','o','o')
+if (x not in string.ascii_lowercase):
+    print("invalid")
+elif(x in v):
+    print("Vowel")
+else:
+    print("consonant")
