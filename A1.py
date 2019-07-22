f19327c71f60b3280f4a3c7cc990d39a300d91a3
dinesh2805 <50877726+dinesh2805@users.noreply.github.com>
2019-07-22 19:09:42 +0530

    Rename Q7.py to A1.py

---
+++ A1.py
@@ -0,0 +1,13 @@
+nm = int(input())
+mylist = list(map(int,input().split()))
+mydict = {}
+anslist = []
+for a in mylist:
+    if a in mydict:
+        mydict[a] += 1
+    else:
+        mydict[a] = 1
+for a in mydict:
+    if mydict[a]==1:
+        anslist.append(a)
+print(*anslist)
